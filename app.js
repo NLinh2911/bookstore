1faf6b4ec486832c2c3e529d3a30324cfe3e3406
chrisph124 <chris.pham124@gmail.com>
2017-08-31 16:58:41 +0700

    render layout 50%

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const index = require('./routes/index')
 
 const app = express()
 
+let post = [
+    {'thumbnail': 'http://www.allitebooks.com/wp-content/uploads/2017/08/Pro-Processing-for-Images-and-Computer-Vision-with-OpenCV.jpg',
+    'title': 'Learning PHP 7',
+    'author': 'Antonio Lopez',
+    'content': 'PHP is a great language for building web applications. It is essentially a server-side scripting language that is also used for general purpose programming. PHP 7 is the latest version with a host of new features, and it provides major backwards-compatibility breaks. This book begins with the fundamentals…' 
+}]
+app.locals.post = post;
 
 // View engine setup
 app.set('views', './views')
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,15 @@ nunjucks.configure('views', {
     autoescape: true,
     express: app
 })
-
+let post = [
+    {'thumbnail': 'http://www.allitebooks.com/wp-content/uploads/2017/08/Pro-Processing-for-Images-and-Computer-Vision-with-OpenCV.jpg',
+    'title': 'Learning PHP 7',
+    'author': 'Antonio Lopez',
+    'content': 'PHP is a great language for building web applications. It is essentially a server-side scripting language that is also used for general purpose programming. PHP 7 is the latest version with a host of new features, and it provides major backwards-compatibility breaks. This book begins with the fundamentals…' 
+}]
 // Home page
 app.get('/', function(req, res, next) {
-    res.render('layout', {title: 'IT Book Store'})
+    res.render('layout', {title: 'IT Book Store', post})
 })
 
 module.exports = app
