170ff660a99d008efeefb01b81e80dbaf2ee520c
chrisph124 <chris.pham124@gmail.com>
2017-09-01 17:10:49 +0700

    created routes

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const index = require('./routes/index')
-
 const app = express()
 
-let post = [
-    {'thumbnail': 'http://www.allitebooks.com/wp-content/uploads/2017/08/Pro-Processing-for-Images-and-Computer-Vision-with-OpenCV.jpg',
-    'title': 'Learning PHP 7',
-    'author': 'Antonio Lopez',
-    'content': 'PHP is a great language for building web applications. It is essentially a server-side scripting language that is also used for general purpose programming. PHP 7 is the latest version with a host of new features, and it provides major backwards-compatibility breaks. This book begins with the fundamentals…' 
-}]
-app.locals.post = post;
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+//Import Routes
+const index = require('./routes/index')
+const detail = require('./routes/detail')
+const author = require('./routes/author')
+
+
+//Template Nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+})
+
 
 // View engine setup
 app.set('views', './views')
@@ -18,5 +25,8 @@ app.set('view engine', 'nunjucks')
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', index)
+app.use('/categories', detail)
+app.use('/author', author)
+
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,29 @@
 const express = require("express");
+const router = express.Router()
 
-const app = express()
+// add a filter
 
-const nunjucks = require('nunjucks')
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
-})
-let post = [
-    {'thumbnail': 'http://www.allitebooks.com/wp-content/uploads/2017/08/Pro-Processing-for-Images-and-Computer-Vision-with-OpenCV.jpg',
+let books = [
+  {
+    'categories': 'Web Development',
+    'category': 'PHP',
+    'thumbnail': 'http://www.allitebooks.com/wp-content/uploads/2017/08/Pro-Processing-for-Images-and-Computer-Vision-with-OpenCV.jpg',
     'title': 'Learning PHP 7',
-    'author': 'Antonio Lopez',
+    'author': ['Harry J.W. Percival', 'James'],
     'content': 'PHP is a great language for building web applications. It is essentially a server-side scripting language that is also used for general purpose programming. PHP 7 is the latest version with a host of new features, and it provides major backwards-compatibility breaks. This book begins with the fundamentals…' 
-}]
+  },
+  {
+    'categories': 'Web Development',
+    'category': 'Python',
+    'thumbnail': 'http://www.allitebooks.com/wp-content/uploads/2017/08/Test-Driven-Development-with-Python-2nd-Edition.jpg',
+    'title': 'Test-Driven Development with Python, 2nd Edition',
+    'author': ['Harry J.W. Percival'],
+    'content': 'By taking you through the development of a real web application from beginning to end, the second edition of this hands-on guide demonstrates the practical advantages of test-driven development (TDD) with Python. You’ll learn how to write and run tests before building each part of your app, and then develop…'
+  }
+]
 // Home page
-app.get('/', function(req, res, next) {
-    res.render('layout', {title: 'IT Book Store', post})
+router.get('/', function(req, res, next) {
+    res.render('layout', {title: 'IT Book Store', books})
 })
 
-module.exports = app
+module.exports = router
