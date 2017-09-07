1bace456f68810fbc6af3c48917d241e50235647
NLinh2911 <nlinh2911@gmail.com>
2017-09-07 22:17:33 +0700

    update data & create api

--- Nunjucks/routes/author.js
+++ Nunjucks/routes/author.js
@@ -4,16 +4,19 @@ const router = express.Router()
 const books = require('../models/book')
 const category = require('../models/category')
 // Detail page
-router.get('/:name', async function(req, res, next) {
-    try {
-        let name = req.params.name.replace(/-/gi, ' ')
-        let getCategory = await category.getCategory()
-        let getBookByAuthor = await books.getBookByAuthor(name) 
-        res.render('index', {title: 'IT Book Store', books: getBookByAuthor, getCategory})
-    }
-    catch (err) {
-      console.log(err);
-    }
-  })
+router.get('/:name', async function (req, res, next) {
+  try {
+    let name = req.params.name.replace(/-/gi, ' ')
+    let getCategory = await category.getCategory()
+    let getBookByAuthor = await books.getBookByAuthor(name)
+    res.render('index', {
+      title: 'IT Book Store',
+      books: getBookByAuthor,
+      getCategory
+    })
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 module.exports = router
