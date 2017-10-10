4f35461988f30e1081c95d281a10a233340924ba
haivx <you@example.com>
2017-10-10 14:20:44 +0700

    first commit

---
+++ VueClient/src/components/addCartButton.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+   <button v-if="isAdding" type="submit" @click="addToCart(title = item)" id="addToCart"><i class="fa fa-cart-plus"></i>  Add to Cart</button>
+   <button v-if="!isAdding" @click="removeFromCart" id="removeToCart"><i class="fa fa-trash"></i>  Remove from Cart</button>
+  </div>
+</template>
+<script>
+ export default {
+   props: ['item'],
+   data () {
+     return {
+       Cart: []
+     }
+   },
+   methods: {
+     addToCart (title) {
+       this.Cart.push(title)
+       console.log('Cart', this.Cart)
+      // return this.Cart
+     }
+   },
+   computed: {
+     isAdding () {
+       return 1
+     }
+   }
+ }
+</script>
+<style coped>
+
+#addToCart{
+    background: #EFDB06;
+    color:black;
+    border: 1px solid #F9E610;
+       padding: 8px 12px;
+       margin-top: 15px;
+       cursor: pointer;
+}
+#addToCart:hover{
+    background: #fff36e;
+    border: 1px solid #fff36e
+}
+#removeToCart{
+     background: #ff3d00;
+    color:white;
+    border: 1px solid #F9E610;
+       padding: 8px 12px;
+       margin-top: 15px;
+       cursor: pointer; 
+}
+#removeToCart:hover{
+  background: #ff3d00;
+}
+</style>
